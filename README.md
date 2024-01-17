6dbcf1c42d8fd942a49721cd13df4d391c660417
Jacob Callahan <jacob.callahan05@gmail.com>
2024-01-17 12:04:02 -0500

    Add the temp_files script
    This script is expected to be ran by cron on some periodic cycle.
    See README and script docstring for full details.

--- README.md
+++ README.md
@@ -40,5 +40,28 @@ It is recommended that your origin point to a private GitHub repository or other
 - Run `crontab -e` to edit your cron file
 - Add an entry for your auto_push.sh script, I recommend `0 17 * * * /path/to/auto_push.sh`
 
+## Temporary Files Management
+We have added a script, temp_files.py, that enables you to create temporary files that are delete when they haven't been **accessed** for a specific amount of time.
+
+The designed use of this script is to periodically be ran by a cron job to remove temporary files
+from a specified directory (TMP_DIRECTORY).
+
+The script will recursively prune all files under TMP_DIRECTORY that haven't been ACCESSED within
+the expiration period (DEFAULT_EXPIRATION or the expiration period for its parent directory).
+
+You can either use the default expiration or nest the temporary files in time-based directories.
+```
+Example:
+    temp/
+    ├── monthly    # Files in this directory will expire after 31 days of not being accessed.
+    │   ├── file1
+    │   └── file2
+    ├── weekly     # Files in this directory will expire after 7 days of not being accessed.
+    │   ├── file1
+    │   └── file2
+```
+
+**Note:** The script should be setup to run with cron (see above).
+
 ## Contributing
 Do you have any great templates or plugins you think are handy? Submit a pull request!
---
+++ temp_files.py
@@ -0,0 +1,69 @@
+"""A script to remove temporary files from a directory.
+
+The designed use of this script is to periodically be ran by a cron job to remove temporary files
+from a specified directory (TMP_DIRECTORY).
+The script will recursively prune all files under TMP_DIRECTORY that haven't been ACCESSED within
+the expiration period (DEFAULT_EXPIRATION or the expiration period for its parent directory).
+You can either use the default expiration or nest the temporary files in time-based directories.
+
+Example:
+    temp/
+    ├── monthly    # Files in this directory will expire after 31 days of not being accessed.
+    │   ├── file1
+    │   └── file2
+    ├── weekly     # Files in this directory will expire after 7 days of not being accessed.
+    │   ├── file1
+    │   └── file2
+"""
+from pathlib import Path
+from datetime import datetime
+
+CHECK_TIME = datetime.now()
+TMP_DIRECTORY = Path("temp")
+DEFAULT_EXPIRATION = 7
+TIME_DICT = {"weekly": 7, "monthly": 31, "quarterly": 123, "yearly": 365}
+
+
+def is_expired(file, expire_period=DEFAULT_EXPIRATION):
+    """
+    Check if a file is expired based on its last accessed date.
+
+    Args:
+        file (Path): The path to the file.
+        expire_period (int, optional): The expiration period in days. Defaults to DEFAULT_EXPIRATION.
+
+    Returns:
+        bool: True if the file is expired, False otherwise.
+    """
+    modified_date = datetime.fromtimestamp(file.stat().st_atime)
+    file_age = CHECK_TIME - modified_date
+    return file_age.days >= expire_period
+
+
+def prune_directory(dir_path, parent_expiration_period=None):
+    """
+    Recursively prunes a directory by deleting expired files.
+
+    Args:
+        dir_path (Path): The path to the directory to be pruned.
+        parent_expiration_period (int, optional): The expiration period in days for the parent directory.
+            If not provided, the default expiration period will be used.
+
+    Returns:
+        None
+    """
+    if not parent_expiration_period:
+        parent_expiration_period = DEFAULT_EXPIRATION
+    # If the directory is a time-based directory, use the expiration period for that directory.
+    # Otherwise, use the expiration period for the parent directory.
+    expire_period = TIME_DICT.get(dir_path.name, parent_expiration_period)
+    for child in dir_path.iterdir():
+        if child.is_dir():
+            prune_directory(child, expire_period)
+        elif child.is_file() and is_expired(child, expire_period):
+                child.unlink()
+
+
+if __name__ == "__main__":
+    if TMP_DIRECTORY.exists():
+        prune_directory(TMP_DIRECTORY)
